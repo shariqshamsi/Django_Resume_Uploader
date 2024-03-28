be3054393a91379e63b41c85f94585ce776e4a58
shariqshamsi <shariqtanzeem@hotmail.com>
2024-03-28 14:34:04 +0500

    This repository contains a Django-based web application for uploading resumes. It utilizes Django's class-based models, forms, and incorporates Bootstrap for styling.

---
+++ resume_uploader/myapp/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+STATE_CHOICE=(
+    ('karachi', 'Karachi'),
+    ('Lahore', 'Lahore'),
+    ('Isl', 'Isl'),
+    ('Rwp', 'Rwp'),
+    ('Mux', 'Mux'),
+)
+
+class Resume(models.Model):
+    name= models.CharField(max_length=100)
+    dob= models.DateField(auto_now=False, auto_now_add=False)
+    gender= models.CharField(max_length=100)
+    locality= models.CharField(max_length=100)
+    city= models.CharField(max_length=100)
+    pin= models.PositiveIntegerField()
+    state= models.CharField(choices=STATE_CHOICE,max_length=50)
+    mobile= models.PositiveIntegerField()
+    email = models.EmailField()
+    job_city = models.CharField(max_length=50)
+    profile_image = models.ImageField(upload_to='profileimg',blank=True)
+    my_file = models.FileField(upload_to='doc',blank=True)
+    
+    
